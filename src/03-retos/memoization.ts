eacab3a1df9231931ee63f0c44a8d7dbe68b104b
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2024-12-07 18:30:32 +0100

    initial commit

---
+++ src/03-retos/memoization.ts
@@ -0,0 +1,11 @@
+const expensiveFunction = () => {
+    console.log("Una única llamada");
+    return 3.1415;
+  }
+  
+  const memoize = (fn) => { };
+  
+  const memoized = memoize(expensiveFunction);
+  console.log(memoized()); // Una única llamada // 3.1415
+  console.log(memoized()); // 3.1415
+  console.log(memoized()); // 3.1415
